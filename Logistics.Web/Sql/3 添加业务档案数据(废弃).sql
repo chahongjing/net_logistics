a08501ae9a2098336c31181c523299e3d7ca8e0e
chahongjing <310510906@qq.com>
2018-10-13 12:31:03 +0800

    完善数据结构

---
+++ Logistics.Web/Sql/3 添加业务档案数据(废弃).sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO dbo.BusinessType(CreatedBy, CreatedOn, ModifiedBy, ModifiedOn, TypeID, Code, ParentID, Name, Time)
+SELECT A.CreatedBy, A.CreatedOn, A.ModifiedBy, A.ModifiedOn, NEWID() AS TypeId,
+       A.Code + '.' + RIGHT('0000' + LiuChengBiao, 4) AS Code, A.TypeID AS ParentID, [F3] AS NAME, ShiJianJianGe AS [time]
+  FROM dbo.BusinessType AS A
+  INNER JOIN LiuChengDanAn AS B ON RTRIM(LTRIM(A.Name)) = RTRIM(LTRIM(SUBSTRING(B.MingCheng, 4, 100)))
